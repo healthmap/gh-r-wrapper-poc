33166b5783be37b5c42e4f55e8c50c945d3dc973
Christopher Remmel <40274075+calremmel@users.noreply.github.com>
2024-05-21 22:46:40 -0400

    add epiline example

--- README.md
+++ README.md
@@ -3,13 +3,25 @@ Contains a proof-of-concepts for wrapping R scripts with Python.
 
 ## Running
 
-Install R packages
+### Install R packages
 ```
 Rscript -e 'install.packages("jsonlite", repos="https://cloud.r-project.org")'
 Rscript -e 'install.packages("arrow", repos="https://cloud.r-project.org")'
 ```
 
-Run python script
+### Install EpiLine
+Follow instructions [here](https://github.com/BDI-pathogens/EpiLine/tree/main?tab=readme-ov-file#installation)
+
+### Install Python virtual environment
+Using Python 3.12:
+```
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+Alternatively, use `poetry` with the included `pyproject.toml`
+
+### Run Python script
 ```
 python <SCRIPT>
 ```
@@ -19,4 +31,4 @@ python <SCRIPT>
 - `log.py` - structured logging
 - `error.py` - error handling and exiting
 - `data.py` - loading data into memory and using it between processes
-
+- `epiline_example.py` - runs model on simulated data, generates plot, returns data
---
+++ epiline_example.py
@@ -0,0 +1,53 @@
+import numpy as np
+import rpy2.robjects as ro
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
+
+
+def load_package():
+    """Loads example EpiLine script."""
+    with open("epiline_example.R") as f:
+        example_str = f.read()
+    example = SignatureTranslatedAnonymousPackage(example_str, "example")
+    return example
+
+
+def run_example(example, t_rep, t_symptom_pre, t_symptom_post, plot_name):
+    """Runs the EpiLine example code.
+
+    Parameters
+    __________
+    example : R Package
+        object representing example EpiLine package
+    t_rep : int
+        length of time for which data is reported
+    t_symptom_pre : int
+        time before the reporting period to simulate
+    t_symptom_post : int
+        time after the reporting period to simulate
+    plot_name : str
+        output filename for symptoms plot. must end in .html
+
+    Results
+    _______
+    fit : R Object
+        object representing fitted model
+
+    """
+    fit = example.run_example(t_rep, t_symptom_pre, t_symptom_post, plot_name)
+    return fit
+
+
+if __name__ == "__main__":
+    PLOT_HTML_FILENAME = "symptoms.html"
+    SYMPTOMS_MATRIX_FILENAME = "symptoms.csv"
+    example = load_package()
+    result = run_example(example, 50, 30, 5, "symptoms.html")
+    symptoms_matrix_r = result["stan_extract"][0]
+    with (ro.default_converter + pandas2ri.converter).context():
+        symptoms_matrix_np = ro.conversion.get_conversion().rpy2py(symptoms_matrix_r)
+    np.savetxt(SYMPTOMS_MATRIX_FILENAME, symptoms_matrix_np, delimiter=",")
+    print("\n-----")
+    print(f"Interactive plot saved at: {PLOT_HTML_FILENAME}")
+    print(f"Symptoms matrix saved at: {SYMPTOMS_MATRIX_FILENAME}")
+    print("-----\n\n")
